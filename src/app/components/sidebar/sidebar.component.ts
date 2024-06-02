f83af5222c1c37723fbfa99a9a16441c201a6f6e
Adeltoumi-Code <adel.toumi@esprit.tn>
2024-06-02 12:35:03 +0100

    feat: conflit fix with appModule

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -12,9 +12,10 @@ export const ROUTES: RouteInfo[] = [
    /* { path: '/maps', title: 'Maps',  icon:'ni-pin-3 text-orange', class: '' },*/
     { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
     { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
+    { path: '/offre', title: 'Gestion des offres',  icon:'ni-bell-55 text-red', class: '' },
     { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
 
-  
+
 ];
 
 @Component({
