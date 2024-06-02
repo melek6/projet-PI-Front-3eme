8a08267f0d9c7108fa9a0433c418d0b243198a54
FatmaFerchichi <135468163+FatmaFerchichi@users.noreply.github.com>
2024-06-02 15:42:10 +0100

    feat:interface Evaluation Formation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
-import { EvaluationfComponent } from './front-pages/EvaluationF/evaluationf.component';
+
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
