baddf2fa7ad3e8dad4758dac5824e1b375715467
FatmaFerchichi <135468163+FatmaFerchichi@users.noreply.github.com>
2024-06-02 13:51:06 +0100

    feat : ajout Evaluation

---
+++ src/app/front-pages/EvaluationF/evaluationf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './evaluationf.component.html',
+  styleUrls: ['./evaluationf.component.css']
+})
+export class EvaluationfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
