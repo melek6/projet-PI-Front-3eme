c986f3c899c4c156dc79ec339037aba897acb6a0
Terzimanel <135385644+Terzimanel@users.noreply.github.com>
2024-06-08 16:04:33 +0200

    auth

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from '../../pages/login/login.component';
 import { RegisterComponent } from '../../pages/register/register.component';
+import { ForgotMDPComponent } from 'src/app/pages/forgot-mdp/forgot-mdp.component';
 
 export const AuthLayoutRoutes: Routes = [
     { path: 'login',          component: LoginComponent },
-    { path: 'register',       component: RegisterComponent }
+    { path: 'register',       component: RegisterComponent },
+    { path: 'resetMDP',       component: ForgotMDPComponent },
+
 ];
---
+++ src/app/pages/forgot-mdp/forgot-mdp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-mdp',
+  templateUrl: './forgot-mdp.component.html',
+  styleUrls: ['./forgot-mdp.component.css']
+})
+export class ForgotMDPComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
