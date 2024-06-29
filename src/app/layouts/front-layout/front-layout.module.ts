e790514ec21e97aab16415684e619f4c5c609d4a
Adeltoumi-Code <adel.toumi@esprit.tn>
2024-06-29 21:12:35 +0100

    feat: update 6

--- src/app/layouts/front-layout/front-layout.module.ts
+++ src/app/layouts/front-layout/front-layout.module.ts
@@ -14,7 +14,7 @@ import { EvaluationfComponent } from 'src/app/front-pages/EvaluationF/evaluation
 import { CommentFormComponent } from 'src/app/front-pages/comment-form/comment-form.component';
 import { ReactFormComponent } from 'src/app/front-pages/react-form/react-form.component';
 import { BlogPostFormComponent } from 'src/app/front-pages/blog-post-form/blog-post-form.component';
-
+import { FormationComponent } from 'src/app/front-pages/Formation/formation.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -34,7 +34,8 @@ import { BlogPostFormComponent } from 'src/app/front-pages/blog-post-form/blog-p
     EvaluationfComponent,
     CommentFormComponent,
     ReactFormComponent,
-    BlogPostFormComponent
+    BlogPostFormComponent,
+    FormationComponent
   ],
   exports: [FrontLayoutComponent]
 })
