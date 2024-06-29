085b48fc69c7749af1a84de5b8bb603769e34162
melek6 <melekbarhoumi58@gmail.com>
2024-06-29 16:57:25 +0100

    feat: update desgin

--- src/app/_services/blog/blog-post.service.ts
+++ src/app/_services/blog/blog-post.service.ts
@@ -13,6 +13,7 @@ export class BlogPostService {
   private apiUrl = 'http://localhost:8081/api/blogposts';
 
   constructor(private http: HttpClient) {}
+
   getAllBlogPosts(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
@@ -22,11 +23,11 @@ export class BlogPostService {
   }
 
   createBlogPost(blogPost: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, blogPost);
+    return this.http.post<any>(this.apiUrl, blogPost, httpOptions);
   }
 
   updateBlogPost(id: number, blogPost: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, blogPost);
+    return this.http.put<any>(`${this.apiUrl}/${id}`, blogPost, httpOptions);
   }
 
   deleteBlogPost(id: number): Observable<void> {
@@ -34,17 +35,40 @@ export class BlogPostService {
   }
 
   addCommentToBlogPost(blogPostId: number, comment: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/${blogPostId}/comments`, comment);
+    return this.http.post<any>(`${this.apiUrl}/${blogPostId}/comments`, comment, httpOptions);
   }
 
   addReactToBlogPost(blogPostId: number, react: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/${blogPostId}/reacts`, react);
+    return this.http.post<any>(`${this.apiUrl}/${blogPostId}/reacts`, react,httpOptions);
   }
+
   getLikesCount(blogPostId: number): Observable<number> {
     return this.http.get<number>(`${this.apiUrl}/${blogPostId}/likes`);
   }
 
   getDislikesCount(blogPostId: number): Observable<number> {
     return this.http.get<number>(`${this.apiUrl}/${blogPostId}/dislikes`);
+  }
+
+  getUserReact(blogPostId: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${blogPostId}/reacts/user`);
+  }
+  getCommentsForBlogPost(postId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/${postId}/comments`);
+  }
+
+  deleteComment(commentId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/deleteComments/${commentId}`, httpOptions);
+  }
+
+  updateComment(commentId: number, content: string): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/comments/${commentId}`, { content }, httpOptions);
+  }
+
+  addReplyToComment(parentId: number, content: string): Observable<any> {
+   return this.http.post<any>(`${this.apiUrl}/comments/${parentId}/reply`, { content }, httpOptions);
+  }
+  rephrase(content: string): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/rephrase`, { content }, httpOptions);
   }
 }
--- src/app/front-pages/blog/blog.component.ts
+++ src/app/front-pages/blog/blog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { BlogPostService } from 'src/app/_services/blog/blog-post.service';
+import { StorageService } from 'src/app/_services/storage.service';
 
 @Component({
   selector: 'app-blog',
@@ -8,51 +8,75 @@ import { BlogPostService } from 'src/app/_services/blog/blog-post.service';
   styleUrls: ['./blog.component.css']
 })
 export class BlogPostComponent implements OnInit {
-  blogPosts: any[];
+  blogPosts: any[] = [];
   showCommentForm: { [key: number]: boolean } = {};
   showReactForm: { [key: number]: boolean } = {};
+  editFormVisible: { [key: number]: boolean } = {};
+  editCommentFormVisible: { [key: number]: boolean } = {};
+  showReplyForm: { [key: number]: boolean } = {};
+  replyContent: { [key: number]: string } = {};
+  createFormVisible: boolean = false;
+  newPost: any = { title: '', content: '' };
 
-  constructor(private blogPostService: BlogPostService, private router: Router) {}
+  constructor(private blogPostService: BlogPostService , private storageService:StorageService) {}
 
   ngOnInit(): void {
     this.blogPostService.getAllBlogPosts().subscribe((data: any[]) => {
       this.blogPosts = data.map(post => ({
         ...post,
-        commentaires: post.commentaires || [], // Initialiser commentaires s'il est undefined
+        commentaires: post.commentaires || [],
         likes: 0,
-        dislikes: 0
+        dislikes: 0,
+        userReact: null
       }));
 
       this.blogPosts.forEach(post => {
         this.blogPostService.getLikesCount(post.id).subscribe(likes => post.likes = likes);
         this.blogPostService.getDislikesCount(post.id).subscribe(dislikes => post.dislikes = dislikes);
+        this.blogPostService.getUserReact(post.id).subscribe(react => post.userReact = react);
+        this.blogPostService.getCommentsForBlogPost(post.id).subscribe(comments => post.commentaires = comments);
       });
     });
   }
 
   likePost(post: any): void {
-    post.likes++;
-    this.blogPostService.updateBlogPost(post.id, post).subscribe(() => {
-      console.log('Post updated:', post);
+    if (post.userReact && post.userReact.type === 'like') {
+      post.userReact = null;
+      post.likes--;
+    } else {
+      if (post.userReact && post.userReact.type === 'dislike') {
+        post.dislikes--;
+      }
+      post.userReact = { type: 'like' };
+      post.likes++;
+    }
+    this.blogPostService.addReactToBlogPost(post.id, post.userReact).subscribe(() => {
+      console.log('Post liked/disliked:', post);
     }, error => {
-      console.error('Error updating post:', error);
+      console.error('Error updating post reaction:', error);
     });
   }
 
   dislikePost(post: any): void {
-    post.dislikes++;
-    this.blogPostService.updateBlogPost(post.id, post).subscribe(() => {
-      console.log('Post updated:', post);
+    if (post.userReact && post.userReact.type === 'dislike') {
+      post.userReact = null;
+      post.dislikes--;
+    } else {
+      if (post.userReact && post.userReact.type === 'like') {
+        post.likes--;
+      }
+      post.userReact = { type: 'dislike' };
+      post.dislikes++;
+    }
+    this.blogPostService.addReactToBlogPost(post.id, post.userReact).subscribe(() => {
+      console.log('Post liked/disliked:', post);
     }, error => {
-      console.error('Error updating post:', error);
+      console.error('Error updating post reaction:', error);
     });
   }
+
   toggleCommentForm(post: any): void {
     this.showCommentForm[post.id] = !this.showCommentForm[post.id];
-  }
-
-  toggleReactForm(post: any): void {
-    this.showReactForm[post.id] = !this.showReactForm[post.id];
   }
 
   onCommentAdded(comment: any): void {
@@ -63,19 +87,93 @@ export class BlogPostComponent implements OnInit {
     }
   }
 
-  onReactAdded(react: any): void {
-    const post = this.blogPosts.find(p => p.id === react.blogPost.id);
-    if (post) {
-      post.reacts = post.reacts || [];
-      post.reacts.push(react);
-    }
+  toggleEditForm(postId: number): void {
+    this.editFormVisible[postId] = !this.editFormVisible[postId];
   }
-  editPost(id: number): void {
-    this.router.navigate(['/edit-blog-post', id]);
+
+  updatePost(post: any): void {
+    this.blogPostService.updateBlogPost(post.id, post).subscribe(() => {
+      console.log('Post updated:', post);
+      this.editFormVisible[post.id] = false;
+    }, error => {
+      console.error('Error updating post:', error);
+    });
   }
+
+  toggleCreateForm(): void {
+    this.createFormVisible = !this.createFormVisible;
+  }
+
+  createPost(): void {
+    this.blogPostService.createBlogPost(this.newPost).subscribe((createdPost: any) => {
+      this.blogPosts.push(createdPost);
+      this.newPost = { title: '', content: '' };
+      this.createFormVisible = false;
+    }, error => {
+      console.error('Error creating post:', error);
+    });
+  }
+
   deletePost(id: number): void {
     this.blogPostService.deleteBlogPost(id).subscribe(() => {
       this.blogPosts = this.blogPosts.filter(post => post.id !== id);
     });
   }
+
+  deleteComment(commentId: number): void {
+    this.blogPostService.deleteComment(commentId).subscribe(() => {
+      this.blogPosts.forEach(post => {
+        post.commentaires = post.commentaires.filter((comment: any) => comment.id !== commentId);
+        post.commentaires.forEach((comment: any) => {
+          comment.replies = comment.replies.filter((reply: any) => reply.id !== commentId);
+        });
+      });
+    });
+  }
+
+  toggleEditCommentForm(commentId: number): void {
+    this.editCommentFormVisible[commentId] = !this.editCommentFormVisible[commentId];
+  }
+
+  updateComment(commentId: number, content: string): void {
+    this.blogPostService.updateComment(commentId, content).subscribe(updatedComment => {
+      this.blogPosts.forEach(post => {
+        post.commentaires = post.commentaires.map((comment: any) => comment.id === commentId ? updatedComment : comment);
+        post.commentaires.forEach((comment: any) => {
+          comment.replies = comment.replies.map((reply: any) => reply.id === commentId ? updatedComment : reply);
+        });
+      });
+      this.editCommentFormVisible[commentId] = false;
+    });
+  }
+
+  toggleReplyForm(commentId: number): void {
+    this.showReplyForm[commentId] = !this.showReplyForm[commentId];
+  }
+
+  addReply(parentId: number, content: string): void {
+    this.blogPostService.addReplyToComment(parentId, content).subscribe(reply => {
+      this.blogPosts.forEach(post => {
+        post.commentaires.forEach((comment: any) => {
+          if (comment.id === parentId) {
+            comment.replies = comment.replies || [];
+            comment.replies.push(reply);
+          }
+        });
+      });
+      this.replyContent[parentId] = '';
+      this.showReplyForm[parentId] = false;
+    });
+  }
+  rephraseContent(): void {
+    this.blogPostService.rephrase(this.newPost.content).subscribe((rephrasedContent: any) => {
+      this.newPost.content = rephrasedContent[0].generated_text;
+    }, error => {
+      console.error('Error rephrasing content:', error);
+    });
+  }
+  userIsOwner(userId: number): boolean {
+    return this.storageService.getUser().id === userId;
+  }
+  
 }
