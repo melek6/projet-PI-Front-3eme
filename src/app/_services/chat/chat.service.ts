377508bb308529900f8fa7735acbe91735cad705
IYEDSAYAHI <86443557+IYEDSAYAHI@users.noreply.github.com>
2024-07-09 21:35:14 +0100

    chat

--- src/app/_services/chat/chat.service.ts
+++ src/app/_services/chat/chat.service.ts
@@ -1,17 +1,14 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Chat, Message } from 'src/app/front-pages/chat/chat.component';
-
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ChatService {
+  private baseUrl = "http://localhost:8081/api/chats";
 
-  private baseUrl = 'http://localhost:8081/api/chats';
-
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   updateChat(message: Message, chatId: string): Observable<Object> {
     return this.httpClient.put(`${this.baseUrl}/message/${chatId}`, message);
@@ -22,10 +19,36 @@ export class ChatService {
   }
 
   createChatRoom(chat: Chat): Observable<Object> {
-    return this.httpClient.post(this.baseUrl + '/add', chat);
+    return this.httpClient.post(this.baseUrl + "/add", chat);
   }
 
-  getChatByFirstUserUsernameOrSecondUserUsername(username: string): Observable<Chat[]> {
+  getChatByUsername(username: string): Observable<Chat[]> {
     return this.httpClient.get<Chat[]>(`${this.baseUrl}/username/${username}`);
   }
+
+  getAllUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(`${this.baseUrl}/users/all`);
+  }
+}
+
+// Models
+export class Chat {
+  id: number;
+  user1: User;
+  user2: User;
+  messageList: Message[];
+}
+
+export class Message {
+  id: number;
+  chat: Chat;
+  senderUsername: string;
+  time: Date;
+  replyMessage: string;
+}
+
+export class User {
+  id: number;
+  username: string;
+  email: string;
 }
--- src/app/front-pages/chat/chat.component.ts
+++ src/app/front-pages/chat/chat.component.ts
@@ -1,158 +1,176 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ChatService } from 'src/app/_services/chat/chat.service';
-import { StorageService } from 'src/app/_services/storage.service';
-import { UserService } from 'src/app/_services/user.service';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ChatService } from "src/app/_services/chat/chat.service";
+import { StorageService } from "src/app/_services/storage.service";
 
 @Component({
-  selector: 'app-chat',
-  templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  selector: "app-chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"],
 })
 export class ChatComponent implements OnInit {
-
   chatForm: FormGroup;
   chatObj: Chat = new Chat();
   messageObj: Message = new Message();
   messageList: Message[] = [];
   chatList: Chat[] = [];
   chatData: Chat;
-  chatId: any = sessionStorage.getItem('chatId');
-  secondUserUsername = '';
-  allUsers: any[] = [];
-  firstUserUsername: string;
+  chatId: any = sessionStorage.getItem("chatId");
+  otherUserUsername = "";
+  allUsers: User[] = [];
+  currentUserUsername: string;
   senderUsername: string;
   senderCheck: string;
 
   constructor(
     private chatService: ChatService,
     private router: Router,
-    private userService: UserService,
     private storageService: StorageService
   ) {
     this.chatForm = new FormGroup({
-      replyMessage: new FormControl()
+      replyMessage: new FormControl(),
     });
 
     const user = this.storageService.getUser();
     if (user) {
-      this.firstUserUsername = user.username;
+      this.currentUserUsername = user.username;
       this.senderUsername = user.username;
       this.senderCheck = user.username;
     }
   }
 
   ngOnInit(): void {
-    if (this.firstUserUsername) {
+    if (this.currentUserUsername) {
       this.loadChats();
       this.loadUsers();
       if (this.chatId) {
         this.loadChatById();
       }
     } else {
-      console.error('User is not logged in.');
-      // Redirect to login page or show a message to the user
-      this.router.navigate(['/login']);
+      console.error("User is not logged in.");
+      this.router.navigate(["/login"]);
     }
   }
 
   loadChats(): void {
     setInterval(() => {
-      if (this.firstUserUsername) {
-        this.chatService.getChatByFirstUserUsernameOrSecondUserUsername(this.firstUserUsername).subscribe(data => {
-          this.chatList = data;
-        }, error => {
-          console.error('Error loading chats:', error);
-        });
+      if (this.currentUserUsername) {
+        this.chatService.getChatByUsername(this.currentUserUsername).subscribe(
+          (data) => {
+            this.chatList = data;
+          },
+          (error) => {
+            console.error("Error loading chats:", error);
+          }
+        );
       }
     }, 1000);
   }
 
   loadUsers(): void {
     setInterval(() => {
-      this.userService.getall().subscribe(data => {
-        this.allUsers = data;
-      }, error => {
-        console.error('Error loading users:', error);
-      });
+      this.chatService.getAllUsers().subscribe(
+        (data) => {
+          this.allUsers = data;
+        },
+        (error) => {
+          console.error("Error loading users:", error);
+        }
+      );
     }, 1000);
   }
 
   loadChatById(): void {
     setInterval(() => {
       if (this.chatId) {
-        this.chatService.getChatById(this.chatId).subscribe((data: Chat) => {
-          this.chatData = data;
-          this.messageList = this.chatData.messageList;
-          this.secondUserUsername = this.chatData.secondUser.username;
-          this.firstUserUsername = this.chatData.firstUser.username;
-        }, error => {
-          console.error('Error loading chat by ID:', error);
-        });
+        this.chatService.getChatById(this.chatId).subscribe(
+          (data: Chat) => {
+            this.chatData = data;
+            this.messageList = this.chatData.messageList;
+            this.otherUserUsername = this.chatData.user2.username;
+            this.currentUserUsername = this.chatData.user1.username;
+          },
+          (error) => {
+            console.error("Error loading chat by ID:", error);
+          }
+        );
       }
     }, 1000);
   }
 
-  loadChatByEmail(event: string, event1: string): void {
-    sessionStorage.removeItem('chatId');
-    this.chatService.getChatByFirstUserUsernameOrSecondUserUsername(event).subscribe((data: Chat[]) => {
-      this.chatData = data.find(chat => chat.secondUser.username === event1 || chat.firstUser.username === event1);
-      if (this.chatData) {
-        this.chatId = this.chatData.chatId.toString(); // Convert number to string
-        sessionStorage.setItem('chatId', this.chatId);
-
-        this.loadChatById();
+  loadChatByUsername(event: string, event1: string): void {
+    sessionStorage.removeItem("chatId");
+    this.chatService.getChatByUsername(event).subscribe(
+      (data: Chat[]) => {
+        this.chatData = data.find(
+          (chat) =>
+            chat.user2.username === event1 || chat.user1.username === event1
+        );
+        if (this.chatData) {
+          this.chatId = this.chatData.id.toString();
+          sessionStorage.setItem("chatId", this.chatId);
+          this.loadChatById();
+        }
+      },
+      (error) => {
+        console.error("Error loading chat by username:", error);
       }
-    }, error => {
-      console.error('Error loading chat by email:', error);
-    });
+    );
   }
 
   sendMessage(): void {
     this.messageObj.replyMessage = this.chatForm.value.replyMessage;
     this.messageObj.senderUsername = this.senderUsername;
-    this.chatService.updateChat(this.messageObj, this.chatId).subscribe(() => {
-      this.chatForm.reset();
-      this.loadChatById();
-    }, error => {
-      console.error('Error sending message:', error);
-    });
+    this.chatService.updateChat(this.messageObj, this.chatId).subscribe(
+      () => {
+        this.chatForm.reset();
+        this.loadChatById();
+      },
+      (error) => {
+        console.error("Error sending message:", error);
+      }
+    );
   }
 
   goToChat(username: string): void {
-    this.chatService.getChatByFirstUserUsernameOrSecondUserUsername(username).subscribe(
+    this.chatService.getChatByUsername(username).subscribe(
       (data: Chat[]) => {
-        const existingChat = data.find(chat => chat.firstUser.username === this.firstUserUsername || chat.secondUser.username === this.firstUserUsername);
+        const existingChat = data.find(
+          (chat) =>
+            chat.user1.username === this.currentUserUsername ||
+            chat.user2.username === this.currentUserUsername
+        );
         if (existingChat) {
-          this.chatId = existingChat.chatId.toString(); // Convert number to string
-          sessionStorage.setItem('chatId', this.chatId);
+          this.chatId = existingChat.id.toString();
+          sessionStorage.setItem("chatId", this.chatId);
         } else {
-          this.chatObj.firstUser = { username: this.firstUserUsername } as User;
-          this.chatObj.secondUser = { username: username } as User;
-          this.chatService.createChatRoom(this.chatObj).subscribe((data: any) => {
-            this.chatData = data;
-            this.chatId = this.chatData.chatId.toString(); // Convert number to string
-            sessionStorage.setItem('chatId', this.chatId);
-          }, err => {
-            console.error('Error creating chat room:', err);
-          });
+          this.chatObj.user1 = { username: this.currentUserUsername } as User;
+          this.chatObj.user2 = { username: username } as User;
+          this.chatService.createChatRoom(this.chatObj).subscribe(
+            (data: any) => {
+              this.chatData = data;
+              this.chatId = this.chatData.id.toString();
+              sessionStorage.setItem("chatId", this.chatId);
+            },
+            (err) => {
+              console.error("Error creating chat room:", err);
+            }
+          );
         }
       },
-      error => {
-        console.error('Error loading chat by username:', error);
+      (error) => {
+        console.error("Error loading chat by username:", error);
       }
     );
   }
 }
 
 // Models
-
 export class Chat {
-  chatId: number;
-  firstUser: User;
-  secondUser: User;
+  id: number;
+  user1: User;
+  user2: User;
   messageList: Message[];
 }
 
