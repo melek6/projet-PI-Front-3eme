c81803dffd605a3005c9545d65be7c43d6b303a1
Adeltoumi-Code <adel.toumi@esprit.tn>
2024-07-12 20:34:47 +0100

    feat: update

--- src/app/front-pages/acceuil/acceuil.component.ts
+++ src/app/front-pages/acceuil/acceuil.component.ts
@@ -1,16 +1,15 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { AuthService } from 'src/app/_services/auth.service';
-import { StorageService } from 'src/app/_services/storage.service';
-import { EventBusService } from 'src/app/_shared/event-bus.service';
 
 @Component({
   selector: 'app-acceuil',
   templateUrl: './acceuil.component.html',
   styleUrls: ['./acceuil.component.css']
 })
+
 export class AcceuilComponent {
+
+  constructor() { }
+
 
 
 }
