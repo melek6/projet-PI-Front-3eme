bbafce7c2871260c3249d52258651ef13e9174e5
melek6 <melekbarhoumi58@gmail.com>
2024-07-13 01:01:23 +0100

    feat: fix conflit

--- src/app/layouts/front-layout/front-layout.module.ts
+++ src/app/layouts/front-layout/front-layout.module.ts
@@ -6,35 +6,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { ClipboardModule } from "ngx-clipboard";
 
-<<<<<<< HEAD
 
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FrontLayoutRoutes } from '../front-layout/front-layout-routing.module';
-import { FrontLayoutComponent } from './front-layout.component';
-import { BlogPostComponent } from 'src/app/front-pages/blog/blog.component';
-import { AcceuilComponent } from 'src/app/front-pages/acceuil/acceuil.component';
-import { EvaluationfComponent } from 'src/app/front-pages/EvaluationF/evaluationf.component';
-import { CommentFormComponent } from 'src/app/front-pages/comment-form/comment-form.component';
-import { ReactFormComponent } from 'src/app/front-pages/react-form/react-form.component';
-import { BlogPostFormComponent } from 'src/app/front-pages/blog-post-form/blog-post-form.component';
-import { FormationComponent } from 'src/app/front-pages/Formation/formation.component';
-import { CandidatComponent } from 'src/app/front-pages/candidat/candidat.component';
-import { OffreCandidatComponent } from 'src/app/front-pages/offre-candidat/offre-candidat.component';
-import { OffreDetailsComponent } from 'src/app/front-pages/offre-details/offre-details.component';
-import { InscritModalComponent } from 'src/app/pages/inscrit-modal/inscrit-modal.component';
-import { UserMarketplaceComponent } from 'src/app/front-pages/user-marketplace/user-marketplace.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { UserProjectsManagementComponent } from 'src/app/front-pages/user-projects-management/user-projects-management.component';
-import { ProjectNotificationsComponent } from 'src/app/front-pages/project-notifications/project-notifications.component';
-import { ChatComponent } from 'src/app/front-pages/chat/chat.component';
-import { QuizComponent } from 'src/app/front-pages/quiz/quiz.component';
-import { QuizDetailsComponent } from 'src/app/front-pages/quiz-details/quiz-details.component';
-import { AddevalComponent } from 'src/app/pages/addeval/addeval.component';
-import { PaymentComponent } from 'src/app/pages/payment/payment.component';
 
-=======
+
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSnackBarModule } from "@angular/material/snack-bar";
@@ -58,7 +32,8 @@ import { ProjectNotificationsComponent } from "src/app/front-pages/project-notif
 import { ChatComponent } from "src/app/front-pages/chat/chat.component";
 import { QuizComponent } from "src/app/front-pages/quiz/quiz.component";
 import { QuizDetailsComponent } from "src/app/front-pages/quiz-details/quiz-details.component";
->>>>>>> 6de693e (Notif update)
+import { AddevalComponent } from "src/app/pages/addeval/addeval.component";
+import { PaymentComponent } from "src/app/pages/payment/payment.component";
 @NgModule({
   imports: [
     CommonModule,
@@ -93,13 +68,10 @@ import { QuizDetailsComponent } from "src/app/front-pages/quiz-details/quiz-deta
     ProjectNotificationsComponent,
     ChatComponent,
     QuizComponent,
-<<<<<<< HEAD
     QuizDetailsComponent,
-    PaymentComponent
-=======
+    PaymentComponent,
 
     QuizDetailsComponent,
->>>>>>> 6de693e (Notif update)
   ],
   exports: [FrontLayoutComponent],
 })
